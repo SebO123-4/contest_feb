fac02365353a17eda89a0ce8fc9633447143d59f
SebO123 <sebastian.e.oertel@gmail.com>
2026-02-17 17:11:23 +0100

    commit

--- decode_start.py
+++ decode_start.py
@@ -4,3 +4,24 @@ message = ""
 with open("secret.txt") as f:
     for line in f:
         print(line) # instead of print, you should start decoding
+        #count vowels
+        #index the alphabet via the vowel count to get letter
+        #collect all letter
+with open("secret.txt") as f:
+    for line in f:
+        count = 0
+
+
+        for char in line.lower():
+            if char in vowel:
+                count += 1
+
+
+        if count < len(alphabet):
+            message += alphabet[count]
+        else:
+
+            message += alphabet[count % len(alphabet)]
+
+print("Decoded message:", message)
+
